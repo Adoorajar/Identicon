545f0c22e48959697013da8bb19d58fcac417abb
Dobbs <Brian.Dobbs@gdit.com>
2021-10-22 10:31:43 -0500

    first commit

---
+++ lib/image.ex
@@ -0,0 +1,6 @@
+defmodule Identicon.Image do
+  @moduledoc """
+    A struct to hold the data structures for our Identicon Image.
+  """
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
+end
